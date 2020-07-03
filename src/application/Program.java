b320c66ae11edd790cf73620b02d9bc33e5d56f3
Mateus Aristides <mateusaristides23@gmail.com>
2020-07-03 11:14:04 -0300

    Calculator (Rectangle)

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
+import entities.Rectangle;
+
 
 public class Program {
 
@@ -12,31 +13,14 @@ public class Program {
 		Locale.setDefault(Locale.US);
     	Scanner sc = new Scanner(System.in);
     	
-    	Product product = new Product();
+    	Rectangle r = new Rectangle();
+    	System.out.println("Enter rectangle width and height:");
     	
-    	System.out.println("Enter product data: ");
-    	System.out.print("Name: ");
-    	product.name = sc.nextLine();
-    	System.out.print("Price: ");
-    	product.price = sc.nextDouble();
-    	System.out.print("Quantity in stock: ");
-    	product.quantity = sc.nextInt();
+    	r.width = sc.nextDouble();
+    	r.height = sc.nextDouble();
     	
-    	System.out.println();
-    	System.out.println("Product data: " + product);
-    	System.out.println();
-    	System.out.print("Enter the number of products to be added in stock: ");
-    	int q = sc.nextInt();
-    	product.addProducts(q);
-    	System.out.println();
-    	System.out.println("Updated data: " + product);
+    	System.out.println(r);
     	
-    	System.out.println();
-    	System.out.print("Enter the number of products do be removed from stock: ");
-    	q = sc.nextInt();
-    	product.removeProducts(q);
-    	System.out.println();
-    	System.out.println("Updated data: " + product);
 	}
 
 }
